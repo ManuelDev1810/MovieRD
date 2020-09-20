53c359a9f8b3e4a55f81d91c56e83d361500e579
Manuel De Jesus Guerrero Vasquez <manueldejesusguerrerovasquez@Manuels-MacBook-Pro.local>
2020-09-19 20:21:22 -0400

    Initial Project

---
+++ ApiPruebaSQlITE/ApiPruebaSQlITE/Models/Hero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiPruebaSQlITE.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SuperHero { get; set; }
+    }
+}
---
+++ ApiPruebaSQlITE/ApiPruebaSQlITE/Models/HeroDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPruebaSQlITE.Models
+{
+    public class HeroDbContext : DbContext
+    {
+        public HeroDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hero> Heroes { get; set; }
+    }
+}
