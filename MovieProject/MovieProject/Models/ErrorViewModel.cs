429012e17941ed8d3a1f211612007f8b4bc7d21f
Manuel De Jesus Guerrero Vasquez <manueldejesusguerrerovasquez@Manuels-MacBook-Pro.local>
2020-09-21 06:51:59 -0400

    Creating the MovieProject

---
+++ MovieProject/MovieProject/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MovieProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    }
+}
